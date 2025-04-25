210f96bd59d13a684321880839d08c4134f94a4f
Harsh <harshgupta20220@gmail.com>
2025-04-25 13:55:34 +0530

    limit

--- server/app.js
+++ server/app.js
@@ -11,7 +11,8 @@ import { putObject } from './controller/upload.js';
 
 const app = express();
 app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 dotenv.config({
